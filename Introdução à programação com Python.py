2f59d8c9802628cfd289bab97252887aabf39b9d
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-08-14 03:33:05 -0300

    adicionando str

--- Introdução à programação com Python.py
+++ Introdução à programação com Python.py
@@ -1,11 +1,11 @@
 a = 10
-b = 5
+b = 6
 soma = a + b
 subtracao = a - b 
 multiplicacao = a * b 
 divisao = a / b 
 resto = a % b
-print(soma)
+print('soma: ' + str(soma))
 print(subtracao)
 print(multiplicacao)
 print(divisao)
