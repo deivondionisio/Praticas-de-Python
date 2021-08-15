6849e2c9f4487e7cfcac8e642ae7c16a93571ba5
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-08-15 02:50:24 -0300

    aula 2 operadores aritmeticos

--- programação com Python DIO/Introdução à programação com Python.py
+++ programação com Python DIO/Introdução à programação com Python.py
@@ -1,13 +1,24 @@
-a = 10
-b = 5
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+print(type(a))
 soma = a + b
 subtracao = a - b 
 multiplicacao = a * b 
 divisao = a / b 
 resto = a % b
-print('soma: ' + str(soma))
-print(subtracao)
-print(multiplicacao)
-print(int(divisao))
-print(divisao)
-print(resto)
+resultado = ('Soma: {soma}. \nSubtracao: {subtracao}. '
+      '\nMultiplicacao: {multiplicacao}'
+      '\nDivisão: {divisao}'
+      '\nResto: {resto}'.format(soma=soma,
+                                subtracao=subtracao,
+                                resto=resto,
+                                multiplicacao=multiplicacao,
+                                divisao=divisao))
+print(resultado)
+
+#print('soma: ' + str(soma))
+#print(subtracao)
+#print(multiplicacao)
+#print(int(divisao))
+#print(divisao)
+#print(resto)
