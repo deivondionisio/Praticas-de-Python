1da22f78aa7834408f2bae32450598c821b6013c
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-10-03 00:50:13 -0300

    exercicio adcional

---
+++ programação com Python DIO/Area do triangulo.py
@@ -0,0 +1,18 @@
+print("-=" * 12)
+print("Analisador de Triângulos")
+print("-=" * 12)
+
+r1 = float(input("Primeiro segmento: "))
+r2 = float(input("Segundo segmento: "))
+r3 = float(input("Teceiro segmento: "))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print("Os segmentos acima podem formar Triângulo", end=' ')
+    if r1 == r2 == r3:
+        print("Equilátero")
+    elif r1 != r2 != r3 != r1:
+        print("Escaleno")
+    else:
+        print("Isósceles")
+else:
+    print("Os segmentos acima não podem formar triângulo")
